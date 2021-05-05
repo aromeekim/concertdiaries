364e8067fda0cf4f388be8e8e27192e95385ce45
Aromee Kim <aromeekim@aromees-mbp.home>
2021-05-04 20:50:05 -0700

    fixes protected routes

--- src/pages/ArtistListPage/ArtistListPage.jsx
+++ src/pages/ArtistListPage/ArtistListPage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-//import './ArtistListPage.css';
 import ArtistListItem from '../../components/ArtistListItem/ArtistListItem';
+// import './PuppyListPage.css';
 
 export default function ArtistListPage(props) {
-  return (
-    <>
-      <h1>Artist List</h1>
-      <div className='ArtistListPage-grid'>
-        {props.artists.map(artist => 
-          <ArtistListItem
-            artist={artist}
-            key={artist._id}
-            handleDeleteArtist={props.handleDeleteArtist}
-          />
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h1>Artist List</h1>
+			<div className='ArtistListPage-grid'>
+				{props.artists.map(artist => (
+					<ArtistListItem
+						artist={artist}
+						key={artist._id}
+						handleDeleteArtist={props.handleDeleteArtist}
+					/>
+				))}
+			</div>
+		</>
+	);
 }
