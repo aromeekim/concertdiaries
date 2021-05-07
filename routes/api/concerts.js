3f486e4b4af1cec35b987a030599483951821dcc
Aromee Kim <aromeekim@aromees-mbp.home>
2021-05-07 08:58:48 -0700

    deletes unused files and folders

--- routes/api/concerts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const concertsCtrl = require('../../controllers/api/concerts');
-
-router.get('/artists/concerts', concertsCtrl.index);
-router.post('/artists/:id/concerts', concertsCtrl.create);
-
-module.exports = router;
